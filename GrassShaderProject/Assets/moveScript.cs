11f99f2151ca45eceb8ec7301eb2fb1a27ea8d08
kyl3r92 <kyl3r@gmx.de>
2019-10-16 23:01:41 +0200

    Improved Demo Scene
    The Movescript (lerping cubes and camera) is now more flexible : you can set a time. Also fixed the problem with them standing still for a while. Vector3.Lerp doesn't like negative values that the sin function gave it. Mathf.Abs to the rescue.

--- GrassShaderProject/Assets/moveScript.cs
+++ GrassShaderProject/Assets/moveScript.cs
@@ -6,6 +6,8 @@ public class moveScript : MonoBehaviour
 {
     public Vector3 start;
     public Vector3 end;
+    [Header("Time to go from start to end")]
+    public float time = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class moveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(start, end, Mathf.Sin(Time.time / 15f));
+        transform.position = Vector3.Lerp(start, end, Mathf.Abs(Mathf.Sin(Time.time / time)));
     }
 }
